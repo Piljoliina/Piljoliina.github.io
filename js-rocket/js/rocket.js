1600270b1e44b8a8b0125ebfc87a967fbcc7f50c
Piljo <Myrskymikael@gmail.com>
2024-05-02 11:20:40 +0300

    rocket upgrading

--- js-rocket/js/rocket.js
+++ js-rocket/js/rocket.js
@@ -13,14 +13,38 @@ var changeState = function (state) {
 
         if (state == 2) {
             timer = setInterval ( function () {
-                countdownNumber = countdownNumber
-                    -1;
+                countdownNumber = countdownNumber-1;
                 document.getElementById('countdown')
                     .innerHTML = countdownNumber;
+                            
+
+
+                            //if (countdownNumber > 4 && countdownNumber <== 7) {
+                                 //TURN UP THE VOLUME!
+
+                                 //document.getElementById('volume') .className = 'volume show';
+
+                                 //}else{
+                                    //document.getElementById('volume') .className = 'volume';
+                                 //}
+                            //};
+
+                             //if (countdownNumber > 1 && countdownNumber <== 4) {
+                                // FUNERAL!
+
+                                //document.getElementById('funeral') .className = 'funeral show';
+
+                                //}else{
+                                    //document.getElementById('volume') .className = 'funeral';
+                                 //}
+                            //}
+
+                            
                             if (countdownNumber <= 0) {
                             changeState  (3);
+
                             };
-                        }, 1000);
+                        }, 200);
                 } else if (state == 3) {
                     var success = setTimeout ( function ()
                         {
